1958a928dcb54ce25680b40aeec6eee77bede012
Letícia Barros <leticiabarros99600512@gmail.com>
2021-12-08 18:27:54 -0300

    ...

--- index.php
+++ index.php
@@ -2,58 +2,48 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Sistema de Login</title>
 </head>
 
 <body>
-    <br>
     <div class="container text-center">
         <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#loginModal">
             Acessar
         </button>
     </div>
 
-    <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="LoginModalLabel" aria-hidden="true">
+    <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-header" style="background-color:#d3d3d3 ;">
-                    <h5 class="modal-title" id="exampleModalLabel">Sistema de Login </h5>
+                <div class="modal-header" style="background-color:cadetblue">
+                    <h5 class="modal-title" id="loginModalLabel">Área Restrita</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form id =login-form> 
+                    <form id="login-usuario-form">
+                        <span id="msgAlertErroLogin"></span>
                         <div class="mb-3">
-                            <label for="email" class="col-form-label">Usuário</label>
-                            <input type="text" name="email" class="form-control" id="email" placeholder="Digite seu Email">
+                            <label for="email" class="col-form-label">Usuário:</label>
+                            <input type="text" name="email" class="form-control" id="email" placeholder="Digite o usuário">
                         </div>
                         <div class="mb-3">
                             <label for="senha" class="col-form-label">Senha:</label>
-                            <input type="password" name="senha" class="form-control" id="senha" placeholder="Digite sua Senha">
+                            <input type="password" name="senha" class="form-control" id="senha" autocomplete="on" placeholder="Digite a senha">
                         </div>
-                        <div class ="mb-3" >
-                        <input type="submit" class ="btn btn-outline-primary bt-sm" id ="login-btn" value ="Entrar">
+                        <div class="mb-3">
+                            <input type="submit" class="btn btn-outline-primary bt-sm" id="login-usuario-btn" value="Acessar">
                         </div>
                     </form>
                 </div>
-                
             </div>
         </div>
     </div>
 
-
-
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <scrip src="js/config.js"></script>
-
+    <script src="js/custom.js"></script>
 </body>
 
 </html>
---
+++ js/custom.js
@@ -0,0 +1,12 @@
+const loginForm = document.getElementById("login-usuario-form");
+const msgAlertErroLogin = document.getElementById("msgAlertErroLogin");
+const loginModal = new bootstrap.Modal(document.getElementById("loginModal"));
+
+loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    if(document.getElementById("email").value ===""){
+        
+    }
+
+});
