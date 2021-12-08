c221957b585f0e07c49951e9f722632a1947b002
Letícia Barros <leticiabarros99600512@gmail.com>
2021-12-08 17:56:14 -0300

    CONEXAO JS

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form>
+                    <form id =login-form> 
                         <div class="mb-3">
                             <label for="email" class="col-form-label">Usuário</label>
                             <input type="text" name="email" class="form-control" id="email" placeholder="Digite seu Email">
@@ -52,6 +52,8 @@
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <scrip src="js/config.js"></script>
+
 </body>
 
 </html>
---
+++ js/conf.js
@@ -0,0 +1,13 @@
+const loginForm = document.getElementById("login-form");
+// const msgAlertErroLogin = document.getElementById("msgAlertErroLogin");
+// const loginModal = new bootstrap.Modal(document.getElementById("loginModal"));
+loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+});
+
+// login-Form, do formulario index.php
+// ESTAMOS TRABALHANDO COM ID POR ISSO O getElementById
+//criando o const para erro de login
+ // usando para não recarregar a página
+// e.preventDefault();
