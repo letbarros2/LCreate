fa967eabb30380bc112726c6d6dcc88fe08c0334
Letícia Barros <leticiabarros99600512@gmail.com>
2021-12-08 17:41:00 -0300

    mudança de pag Inicial

--- index.php
+++ index.php
@@ -3,52 +3,52 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
     <title>Sistema de Login</title>
 </head>
 
 <body>
-    <!-- Criando Div para botão de login -->
-
+    <br>
     <div class="container text-center">
-        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#LoginModal">
-            Acessar o Portal
+        <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#loginModal">
+            Acessar
         </button>
     </div>
 
-    <!-- Criando Modal -->
-    <div class="modal fade" id="LoginModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="LoginModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-header">
+                <div class="modal-header" style="background-color:#d3d3d3 ;">
                     <h5 class="modal-title" id="exampleModalLabel">Sistema de Login </h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <!-- Formulario de Login com Usuário e senha -->
-                    <form id ="login-form">
+                    <form>
                         <div class="mb-3">
-                            <label for="usuario" class="col-form-label">Usuário:</label>
-                            <input type="text" name="usuario" placeholder="Digite o seu Email  📧" class="form-control" id="usuario">
+                            <label for="email" class="col-form-label">Usuário</label>
+                            <input type="text" name="email" class="form-control" id="email" placeholder="Digite seu Email">
                         </div>
                         <div class="mb-3">
-                            <label for="senha" class="col-form-label">Senha</label>
-                            <input type="password" name="senha" placeholder="Digite a sua Senha" class="form-control" id="senha">
+                            <label for="senha" class="col-form-label">Senha:</label>
+                            <input type="password" name="senha" class="form-control" id="senha" placeholder="Digite sua Senha">
                         </div>
-                        <!-- Botão para iniciar o Login do Formulario -->
-                        <div class="mb-3">
-                            <input type="submit" class="btn btn-outline-danger bt-sm" id="btnlogin" value="Acessar">
+                        <div class ="mb-3" >
+                        <input type="submit" class ="btn btn-outline-primary bt-sm" id ="login-btn" value ="Entrar">
                         </div>
                     </form>
                 </div>
-
+                
             </div>
         </div>
     </div>
+
+
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
