a57c4075650f92867bc31e0a444118322e546b35
Letícia Barros <leticiabarros99600512@gmail.com>
2021-12-08 19:21:51 -0300

    CRIANDO AJUSTES DE USSER

--- js/custom.js
+++ js/custom.js
@@ -5,17 +5,17 @@ const loginModal = new bootstrap.Modal(document.getElementById("loginModal"));
 loginForm.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-    if(document.getElementById("email").value ===""){
-        msgAlertErroLogin.innerHTML="<div class='alert alert-danger' role='alert'> ⚠️ Erro: Olá, identificamos que seu usuário está errado, favor tentar novamente </div>";
-    }else if (document.getElementById("senha").value===""){
-        msgAlertErroLogin.innerHTML="<div class='alert alert-danger' role='alert'> ⚠️ Erro: Olá, identificamos que sua senha está incorreta, favor tentar novamente </div>";
-    }else{
-        const dadosForm =new FormData(loginForm);
-        await fetch ("validacao.php",{
-            
+    if (document.getElementById("email").value === "") {
+        msgAlertErroLogin.innerHTML = "<div class='alert alert-danger' role='alert'> ⚠️ Erro: Olá, identificamos que seu usuário está errado, favor tentar novamente </div>";
+    } else if (document.getElementById("senha").value === "") {
+        msgAlertErroLogin.innerHTML = "<div class='alert alert-danger' role='alert'> ⚠️ Erro: Olá, identificamos que sua senha está incorreta, favor tentar novamente </div>";
+    } else {
+        const dadosForm = new FormData(loginForm);
+        await fetch("validacao.php", {
+            method: 'POST',
+            body: dadosForm
+         });
 
-        }
-     
-        )};
+    }
 
 });
