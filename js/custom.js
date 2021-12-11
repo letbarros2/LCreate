472e9455ee61a02667c967e8a10332423910d8fc
Letícia Barros <leticiabarros99600512@gmail.com>
2021-12-11 18:36:00 -0300

    manipulação de validação

--- js/custom.js
+++ js/custom.js
@@ -5,22 +5,28 @@ const loginModal = new bootstrap.Modal(document.getElementById("loginModal"));
 loginForm.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-    // if (document.getElementById("email").value === "") {
-    //     msgAlertErroLogin.innerHTML = "<div class='alert alert-danger' role='alert'> ⚠️ Erro: Olá, identificamos que seu usuário está errado, favor tentar novamente </div>";
-    // } else if (document.getElementById("senha").value === "") {
-    //     msgAlertErroLogin.innerHTML = "<div class='alert alert-danger' role='alert'> ⚠️ Erro: Olá, identificamos que sua senha está incorreta, favor tentar novamente </div>";
-    // } else {
-        
-
+    if (document.getElementById("email").value === "") {
+        msgAlertErroLogin.innerHTML = "<div class='alert alert-danger' role='alert'> ⚠️ Erro: Olá, identificamos que seu usuário está errado, favor tentar novamente </div>";
+    } else if (document.getElementById("senha").value === "") {
+        msgAlertErroLogin.innerHTML = "<div class='alert alert-danger' role='alert'> ⚠️ Erro: Olá, identificamos que sua senha está incorreta, favor tentar novamente </div>";
+    } else {
         const dadosForm = new FormData(loginForm);
-        await fetch("validacao.php", {
+
+         const  dados = await fetch("validacao.php", {
             method: "POST",
             body: dadosForm
         });
         const resposta = await dados.json();
 
-        console.log(resposta);
+        if(resposta['erro']){
+            msgAlertErroLogin.innerHTML = resposta['msg']
+        }else{
+            document.getElementById("dados_usuarios").innerHTML = "Bem vindo " + resposta['dados'].nome + "<br><a href='sair.php'>Sair</a><br>";
+            loginForm.reset();
+            loginModal.hide();
 
-    //}
+        }
+
+    }
 
 });
