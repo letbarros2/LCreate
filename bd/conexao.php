78eab7390277077948cd858421ebdd11c69ad281
Letícia Barros <leticiabarros99600512@gmail.com>
2021-12-11 18:36:13 -0300

    conexão banco local

--- bd/conexao.php
+++ bd/conexao.php
@@ -1,5 +1,19 @@
-<!-- CREATE DATABASE PROJECT;
-USE PROJECT 
+<?php
 
-CRIAÇÃO DE TABELA
-CREATE TABLE `project`.`usuarios` ( `id` INT NOT NULL AUTO_INCREMENT , `nome` VARCHAR(220) NOT NULL , `email` VARCHAR(220) NOT NULL , `senha` VARCHAR(220) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB; -->
+$host = "localhost";
+$user ="root";
+$pass = "";
+$dbname ="project";
+$port = 3306;
+
+try{
+    // $conn = new PDO("mysql:host=$host;port=$port;dbname=" .$dbname, $user, $pass);
+    // echo "<div class='alert alert-success' role='alert'> ✔ CONEXÃO  BD REALIZADA COM SUCESSO </div>";
+    $conn = new PDO("mysql:host=$host;dbname=" . $dbname,$user,$pass);
+
+}catch(Exception $erro){
+    echo "<div class ='alert alert-danger' role'alert'> ❌ CONEXÃO FALHOU </div>".$erro->getMessage();
+}
+
+
+?>
